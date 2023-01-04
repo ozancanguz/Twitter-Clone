4a4d4ca2f27b49af62eb4334c6a13c694dcf66a1
Ozan <ozancangu24@gmail.com>
2023-01-04 12:35:34 +0300

    -login screen designed

--- app/src/main/java/com/ozancanguz/twitter_clone/ui/login/LoginActivity.kt
+++ app/src/main/java/com/ozancanguz/twitter_clone/ui/login/LoginActivity.kt
@@ -1,12 +1,17 @@
-package com.ozancanguz.twitter_clone
+package com.ozancanguz.twitter_clone.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ozancanguz.twitter_clone.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+
+
+
 
 
 
