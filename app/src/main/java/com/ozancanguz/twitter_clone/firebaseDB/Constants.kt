195a3eb60793b1e23a67c4aea896abad6e6bcdec
Ozan <ozancangu24@gmail.com>
2023-01-05 13:11:31 +0300

    -tweet saved in firebase firestore database

--- app/src/main/java/com/ozancanguz/twitter_clone/firebaseDB/Constants.kt
+++ app/src/main/java/com/ozancanguz/twitter_clone/firebaseDB/Constants.kt
@@ -13,6 +13,13 @@ class Constants {
         val REQUEST_CODE_PHOTO=345
         val DATA_IMAGES = "ProfileImages"
 
+        val DATA_TWEETS = "Tweets"
+        val DATA_TWEET_USER_IDS = "userIds"
+        val DATA_TWEET_HASHTAGS = "hashtags"
+        val DATA_TWEETS_LIKES = "likes"
+
+        val DATA_TWEET_IMAGES = "TweetImages"
+
 
 
     }
--- app/src/main/java/com/ozancanguz/twitter_clone/ui/tweetScreen/TweetActivity.kt
+++ app/src/main/java/com/ozancanguz/twitter_clone/ui/tweetScreen/TweetActivity.kt
@@ -4,13 +4,24 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import com.ozancanguz.twitter_clone.R
 import com.ozancanguz.twitter_clone.databinding.ActivitySignUpBinding
 import com.ozancanguz.twitter_clone.databinding.ActivityTweetBinding
+import com.ozancanguz.twitter_clone.firebaseDB.Constants.Companion.DATA_TWEETS
 import com.ozancanguz.twitter_clone.firebaseDB.Constants.Companion.REQUEST_CODE_PHOTO
+import com.ozancanguz.twitter_clone.firebaseDB.Tweet
 
 class TweetActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTweetBinding
+
+    private val firebaseDB = FirebaseFirestore.getInstance()
+    private val firebaseStorage = FirebaseStorage.getInstance().reference
+    private var imageUrl: String? = null
+    private var userId: String? = null
+    private var userName: String? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +30,16 @@ class TweetActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        // getting parameters
+        if(intent.hasExtra(PARAM_USER_ID) && intent.hasExtra(PARAM_USER_NAME)) {
+            userId = intent.getStringExtra(PARAM_USER_ID)
+            userName = intent.getStringExtra(PARAM_USER_NAME)
+        } else {
+            Toast.makeText(this, "Error creating tweet", Toast.LENGTH_SHORT).show()
+            finish()
+        }
 
+        postTweet()
     }
 
     companion object{
@@ -37,6 +57,26 @@ class TweetActivity : AppCompatActivity() {
 
     fun addImage(){
 
+    }
+
+    fun postTweet(){
+        binding.fabSend.setOnClickListener {
+            val text = binding.tweetText.text.toString()
+            val hashtags =getHashtags(text)
+
+            val tweetId = firebaseDB.collection(DATA_TWEETS).document()
+            val tweet = Tweet(tweetId.id, arrayListOf(userId!!), userName, text, imageUrl, System.currentTimeMillis(), hashtags, arrayListOf())
+            tweetId.set(tweet)
+                .addOnCompleteListener { finish() }
+                .addOnFailureListener { e ->
+                    e.printStackTrace()
+
+                    Toast.makeText(this, "Failed to post the tweet.", Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
+    fun getHashtags(source: String): ArrayList<String> {
+        return arrayListOf()
     }
 
 
